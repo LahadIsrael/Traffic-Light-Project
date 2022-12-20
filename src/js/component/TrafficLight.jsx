b1e2a075fe0a65e0f892c4808e7f937c36dcc031
michbalkany <askmitchb@gmail.com>
2022-12-20 20:24:38 +0000

    completed functionality switching colors

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 
 
 
 
 //create your first component
 const TrafficLight = () => {
+	const [ color, setColor] = useState("red");
+
 	return (
 	<>
 	<div className="cable mx-auto"></div>
 	<div className="box d-flex flex-column align-items-center mx-auto">
-		<div className='red light'></div>
-		<div className='yellow light'></div>
-		<div className='green light'></div>
+		<div className={color === 'red' ? 'red light glow': 'red light'} onClick={() => setColor("red")}></div>
+		<div className={color === 'yellow'? 'yellow light glow' : 'yellow light'} onClick={() => setColor('yellow')}></div>
+		<div className={color=== 'green' ? 'green light glow' : 'green light'} onClick={() => setColor('green')}></div>
 		
 	</div>
 	</>	
